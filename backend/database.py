1882690a6bdc70f653014286a4c716c2b33de8d5
FreakyAdy <urban.void.69@gmail.com>
2026-01-09 15:01:59 +0530

    deployment error fix

--- backend/database.py
+++ backend/database.py
@@ -1,8 +1,13 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./mindrepo.db"
+if os.environ.get("VERCEL"):
+    SQLALCHEMY_DATABASE_URL = "sqlite:////tmp/mindrepo.db"
+else:
+    SQLALCHEMY_DATABASE_URL = "sqlite:///./mindrepo.db"
+
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
